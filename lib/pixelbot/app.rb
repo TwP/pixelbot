a052e89d632d7c8b31afb4a8fc9559a27eab7c5f
Tim Pease <tim.pease@gmail.com>
2015-12-08 21:23:42 -0700

    methods for setting brightness and color

--- lib/pixelbot/app.rb
+++ lib/pixelbot/app.rb
@@ -16,6 +16,28 @@ module Pixelbot
       "hello from '#{`uname -a`}'"
     end
 
+    post "/brightness" do
+      content_type :json
+
+      response = params[:brightness] ?
+          {:brightness => get_value(:brightness)} : {}
+
+      MultiJson.dump response
+    end
+
+    post "/set_color" do
+      content_type :json
+      MultiJson.dump \
+        :red   => get_value(:red),
+        :green => get_value(:green),
+        :blue  => get_value(:blue)
+    end
+
+    get "/tmp" do
+      content_type :html
+      erb :index
+    end
+
     get "/hello" do
       "Hello World"
     end
@@ -26,6 +48,11 @@ module Pixelbot
         puts "My work here is done"
       end
       "I'm doing work in the background, but I am still free to take requests"
+    end
+
+    def get_value( name )
+      value = Integer(params[name])
+      value = value.abs & 0xff
     end
   end
 end
